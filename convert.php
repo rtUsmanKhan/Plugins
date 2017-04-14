fe2335e3a5db8825f9d4a3cf818f8f1ee888a443
Usman Khan <usman.khan@RT-DT-516.rolustech.net>
2017-04-14 12:44:24 +0500

    Converter

---
+++ convert.php
@@ -0,0 +1,38 @@
+<?php
+$username = "root";
+$password = "123";
+$hostname = "localhost"; 
+
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password) 
+ or die("Unable to connect to MySQL");
+echo "Connected to MySQL<br>";
+
+//select a database to work with
+$selected = mysql_select_db("sugar7",$dbhandle) 
+  or die("Could not select examples");
+
+$module = 'cases';
+$sql = "SELECT * from $module";
+	$result = mysql_query($sql);
+
+        //create an array
+        $emparray = array();
+        while($row =mysql_fetch_assoc($result))
+        {
+            $emparray[] = $row;
+        }
+        echo json_encode($emparray);
+        $fp = fopen($module, 'w');
+        fwrite($fp, json_encode($emparray));
+      fclose($fp);
+mysql_close($dbhandle);
+/*
+select *
+from  accounts
+where date_modified >= DATE_SUB(NOW(),INTERVAL 8 HOUR)
+
+
+UPDATE tableName SET columnName = FLOOR( 1 + RAND( ) *3 );
+*/
+?>
