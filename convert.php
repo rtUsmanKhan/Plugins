98f1b4d06a178ccb9fda3caa7d5a77b5f9aac5c8
Usman Khan <usman.khan@RT-DT-516.rolustech.net>
2017-04-25 17:23:17 +0500

    diff merge now

--- convert.php
+++ convert.php
@@ -2,16 +2,21 @@
 $username = "root";
 $password = "123";
 $hostname = "My Local Host"; 
+
 //connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password) 
  or die("Unable to connect to MySQL");
 echo "Connected to MySQL<br>";
+
 //select a database to work with
 $selected = mysql_select_db("sugar7",$dbhandle) 
+
   or die("Could not select examplez");
+
 $module = 'cases';
 $sql = "SELECT * from $module";
 	$result = mysql_query($sql);
+
         //create an array
         $emparray = array();
         while($row =mysql_fetch_assoc($result))
@@ -23,6 +28,15 @@ $sql = "SELECT * from $module";
         fwrite($fp, json_encode($emparray));
       fclose($fp);
 mysql_close($dbhandle);
+
+/*
+select *
+from  accounts
+where date_modified >= DATE_SUB(NOW(),INTERVAL 8 HOUR)
+
+
+UPDATE tableName SET columnName = FLOOR( 1 + RAND( ) *3 );
+*/
 // Just to see to staged
 // Lets try again with diffmerge tool
 echo "Hello";
