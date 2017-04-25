30f0e9a16bdd5d56daf6d13b1db419dcdb03ad00
Usman Khan <usman.khan@RT-DT-516.rolustech.net>
2017-04-25 15:44:29 +0500

    try diffmerge tool

--- convert.php
+++ convert.php
@@ -1,14 +1,14 @@
 <?php
 $username = "root";
 $password = "123";
-$hostname = "My Host"; 
+$hostname = "My Local Host"; 
 //connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password) 
  or die("Unable to connect to MySQL");
 echo "Connected to MySQL<br>";
 //select a database to work with
 $selected = mysql_select_db("sugar7",$dbhandle) 
-  or die("Could not select examples");
+  or die("Could not select examplez");
 $module = 'cases';
 $sql = "SELECT * from $module";
 	$result = mysql_query($sql);
